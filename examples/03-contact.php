37609117aa45db60547a961dd38563fbc98c52af
Jakub Bouček <pan@jakubboucek.cz>
2018-08-31 04:10:53 +0200

    Examples: Přejmenování souborů

--- examples/03-contact.php
+++ examples/03-contact.php
@@ -1,6 +1,6 @@
 <?php
 
-include(__DIR__.'/config.php');
+include(__DIR__ . '/00-config.php');
 
 $vyfakturuj_api = new VyfakturujAPI(VYFAKTURUJ_API_LOGIN,VYFAKTURUJ_API_KEY);
 
@@ -15,10 +15,10 @@ $vyfakturuj_api = new VyfakturujAPI(VYFAKTURUJ_API_LOGIN,VYFAKTURUJ_API_KEY);
 #####################################################################################
 #
 #
-// Vytvoříme nový kontakt
-$opt_contact = array(
+
+$opt = array(
     'IC' => '123456789',
-    'name' => '#API - Ukázkový kontakt pro pravidelnou fakturu',// "#API - " dáváme na začátek, chceme mít tento kontakt na začátku našeho adresáře
+    'name' => '#API - Ukázkový kontakt',// "#API - " dáváme na začátek, chceme mít tento kontakt na začátku našeho adresáře
     'note' => 'Kontakt vytvořený přes API',
     'company' => 'Ukázkový kontakt',
     'street' => 'Pouliční 79/C',
@@ -27,37 +27,13 @@ $opt_contact = array(
     'country' => 'Česká republika',
     'mail_to' => 'info@examle.com',
 );
-$contact = $vyfakturuj_api->createContact($opt_contact);    // vytvoříme nový kontakt
 
-$_ID_CONTACT = $contact['id'];
+$contact = $vyfakturuj_api->createContact($opt);    // vytvoříme novou fakturu
 
-$opt_template = array(
-    'id_customer' => $_ID_CONTACT,// vložíme právě vytvořený kontakt
-//    'id_customer' => 20224,// vložíme právě vytvořený kontakt
-    'type' => 2,// chceme pravidelnou fakturu
-    'name' => '#API - Test pravidelné faktury',
-//    'id_customer' => $contact['id'],// vložíme právě vytvořený kontakt
-    'items' => array(
-        array(
-            'text' => 'Stěrač na ponorku',
-            'unit_price' => 990.25,
-            'vat_rate' => 15,
-        ),
-        array(
-            'text' => 'Kapalina do ostřikovačů 250 ml',
-            'unit_price' => 59,
-            'vat_rate' => 15,
-        )
-    )
-);
+echo '<h2>Vytvořili jsme kontakt:</h2>';
+echo '<pre>'.print_r($contact,true).'</pre>';
 
-
-$ret = $vyfakturuj_api->createTemplate($opt_template);    // vytvoříme novou fakturu
-
-echo '<h2>Vytvořili jsme pravidelnou fakturu:</h2>';
-echo '<pre>'.print_r($ret,true).'</pre>';
-
-$_ID_ITEM = $ret['id'];    // uložíme si ID nového zaznamu
+$_ID_KONTAKTU = $contact['id'];    // uložíme si ID nového kontaktu
 #
 #
 #####################################################################################
@@ -70,26 +46,14 @@ $_ID_ITEM = $ret['id'];    // uložíme si ID nového zaznamu
 #
 #
 
-$opt_template = array(
-    'name' => '#API + Test pravidelné faktury',// změníme název
-    'items' => array(
-        array(
-            'text' => 'Stěrač na ponorku',
-            'unit_price' => 990.25,
-            'vat_rate' => 21,// změníme DPH
-        ),
-        array(
-            'text' => 'Kapalina do ostřikovačů 250 ml',
-            'unit_price' => 59,
-            'vat_rate' => 21,// změníme DPH
-        )
-    )
+$opt = array(
+    'name' => '#API - Ukázkový kontakt - po úpravě',//
 );
 
-$ret2 = $vyfakturuj_api->updateTemplate($_ID_ITEM,$opt_template);    // upravíme zaznam
+$contact2 = $vyfakturuj_api->updateContact($_ID_KONTAKTU,$opt);    // upravíme kontakt
 
-echo '<h2>Upravili jsme pravidelnou fakturu:</h2>';
-echo '<pre>'.print_r($ret2,true).'</pre>';
+echo '<h2>Upravili jsme fakturu:</h2>';
+echo '<pre>'.print_r($contact2,true).'</pre>';
 
 
 #
@@ -105,12 +69,14 @@ echo '<pre>'.print_r($ret2,true).'</pre>';
 #
 
 
-$ret3 = $vyfakturuj_api->getTemplate($_ID_ITEM);
-// $ret3 = $vyfakturuj_api->getTemplates(); // vrati vsechny sablony a pravidelné faktury
-// $ret3 = $vyfakturuj_api->getTemplates(array('type' => 2,'end_type' => 1)); // vrati vsechny pravidelné faktury, které nemají nastaveno datum ukončení
+$contact3 = $vyfakturuj_api->getContact($_ID_KONTAKTU); // načte 1 konkrétní kontakt
+// $contact3 = $vyfakturuj_api->getContacts();  // vrátí všechny moje kontakty
 
-echo '<h2>Načetli jsme data o pravidelné faktuře faktuře ze systému:</h2>';
-echo '<pre>'.print_r($ret3,true).'</pre>';
+echo '<h2>Načetli jsme data o kontaktu ze systému:</h2>';
+echo '<pre>'.print_r($contact3,true).'</pre>';
+
+
+
 
 
 #
@@ -124,12 +90,13 @@ echo '<pre>'.print_r($ret3,true).'</pre>';
 #####################################################################################
 #
 #
+
 exit;   // zablokování smazání
 
-$ret4 = $vyfakturuj_api->deleteTemplate($_ID_ITEM);
-$ret5 = $vyfakturuj_api->deleteContact($_ID_CONTACT);
+$contact4 = $vyfakturuj_api->deleteContact($_ID_KONTAKTU);
 
-echo '<h2>Načetli jsme data o průběhu smazání faktury ze systému:</h2>';
-echo '<pre>'.print_r($ret4,true).'</pre>';
-echo '<pre>'.print_r($ret5,true).'</pre>';
+echo '<h2>Načetli jsme data o průběhu smazání kontaktu ze systému:</h2>';
+echo '<pre>'.print_r($contact4,true).'</pre>';
+
+
 exit;
--- examples/README.md
+++ examples/README.md
@@ -1,67 +1,69 @@
 # Příklady použití [PHP knihovny Vyfakturuj API](https://github.com/vyfakturuj/vyfakturuj-api-php)
 
 ## Příprava pro použití příkladů
-V na začátku souboru [/examples/config.php](config.php) doplňte na označené místo Váš `login` a `API klíč`,
+Aby Vám níže uvedené příklady fungovaly, nastavte si nejdříve přihlášení do vašeho účtu. 
+
+V souboru [00-config.php](00-config.php) doplňte na označené místo Váš `login` a `API klíč`,
 které najdete v [nastavení API ve Vyfakturuj](https://app.vyfakturuj.cz/nastaveni/api/).
  
 ## Příklady
 
 ### 1. Test spojení na server
-> Soubor: [/examples/1-test.php](1-test.php)
+> Soubor: [01-test.php](01-test.php)
 
 Otestuje připojení na server (musí se zobrazit welcome zpráva s datumem).
 
 ### 2. Faktury
-> Soubor: [/examples/2-invoice.php](2-invoice.php)
+> Soubor: [02-invoice.php](02-invoice.php)
 
 Ukázka, jak vytvářet, updatovat, získávat a mazat faktury.
 
 ### 3. Kontakty
-> Soubor: [/examples/3-contact.php](3-contact.php)
+> Soubor: [03-contact.php](03-contact.php)
 
 Ukázka, jak vytvářet, updatovat, získávat a mazat kontakty.
 
 ### 4. Šablony
-> Soubor: [/examples/4-template.php](4-template.php)
+> Soubor: [04-template.php](04-template.php)
 
 Ukázka, jak vytvářet, updatovat, získávat a mazat pravidelné faktury a šablony.
 
 ### 5. Odeslání e-mailu s fakturou
-> Soubor: [/examples/5-invoice-sendMail.php](5-invoice-sendMail.php)
+> Soubor: [05-invoice-sendMail.php](05-invoice-sendMail.php)
 
 Ukázka, jak odeslat e-mail s fakturou.
 
 ### 6. Uhrazení faktury
-> Soubor: [/examples/6-invoice-setPayment.php](6-invoice-setPayment.php)
+> Soubor: [06-invoice-setPayment.php](06-invoice-setPayment.php)
 
 Ukázka, jak provést uhrazení faktury.
 
 ### 7. Zobrazení PDF
-> Soubor: [/examples/7-test-invoice-download.php](7-test-invoice-download.php)
+> Soubor: [07-test-invoice-download.php](07-test-invoice-download.php)
 
 Ukázka, jak pracovat s funkci `test_invoice__asPdf()`. Tato funkce vrátí PDF aniž by uložila dokument (fakturu) do systému. Hodí se zejména pokud potřebujeme odladit vzhled faktury.
 
 ### 8. EET
-> Soubor: [/examples/8-invoice-sendEET.php](8-invoice-sendEET.php)
+> Soubor: [08-invoice-sendEET.php](08-invoice-sendEET.php)
 
 Ukázka, jak poslat požadavek na zaúčtování dokladu v EET.
 
 ### 9. Vyhledávání faktur
-> Soubor: [/examples/9-invoice_search.php](9-invoice_search.php)
+> Soubor: [09-invoice_search.php](09-invoice_search.php)
 
 Ukázka, jak vyhledávat v seznamu faktur.
 
 ### 10. Produkty
-> Soubor: [/examples/a-product.php](a-product.php)
+> Soubor: [10-product.php](10-product.php)
 
 Ukázka, jak vyhledávat v seznamu produktů (SimpleShop.cz).
 
 ### 11. Platební metody
-> Soubor: [/examples/b-payment-method.php](b-payment-method.php)
+> Soubor: [11-payment-method.php](11-payment-method.php)
 
 Ukázka, jak získat seznam platebních metod (způsobů uhrady).
 
 ### 12. Číselné řady
-> Soubor: [/examples/c-number-series.php](c-number-series.php)
+> Soubor: [12-number-series.php](12-number-series.php)
 
 Ukázka, jak získat seznam číslených řad.
