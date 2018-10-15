d4352416fc9d116150787eafc2893c0dc276b1ca
Jakub Bouček <pan@jakubboucek.cz>
2018-10-15 17:36:40 +0200

    Fix simple-autoloader (remove forgotten debug code)

--- simple-autoload.php
+++ simple-autoload.php
@@ -12,7 +12,6 @@
  * @see https://github.com/redbitcz/vyfakturuj-api-php/blob/master/manual-installation.md (Nápověda)
  */
 spl_autoload_register(function ($class) {
-    echo $class;
     $file = __DIR__ . '/src/' . str_replace('Redbit\\Vyfakturuj\\Api\\', '', $class) . '.php';
     if (file_exists($file)) {
         require $file;
